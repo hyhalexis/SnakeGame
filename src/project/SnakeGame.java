d1c01bf9caa57b8cba5daf6c3b8d3ef53889a34a
Mariah Yelenick <31373476+mariahyelenick@users.noreply.github.com>
2020-04-22 15:44:03 -0500

    Initial commit

---
+++ src/project/SnakeGame.java
@@ -0,0 +1,13 @@
+package project;
+
+
+public class SnakeGame {
+	
+	//TODO: Implement the game of snake
+	//		You may add other files/classes, but the game should start by running this file
+	public static void main(String[] args) {
+		System.out.println("I'm a snaaaaaaaake! HISSSSSSSSSSSssssss.....");
+		// TODO: Run the game from here!
+	}
+	
+}
